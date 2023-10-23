237f2b8fdacbbdaae4a09bdb2e70bdca38dab948
Jeffmwai <Jefylionel@gmail.com>
2023-10-23 21:07:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+// Define the findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Define the fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // Define the matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export the functions to be used in your tests
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName,
+  };
+  
